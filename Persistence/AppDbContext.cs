25fe6a4ae3771fdae1d7c7dae18f17305ed6b50c
Ajith Kumara <nmpajith@gmail.com>
2025-11-14 16:38:25 +0530

    Initial Create

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options): DbContext (options)
+{
+    public required DbSet<Activity> Activities {get; set; }
+}
